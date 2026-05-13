24f369aaa969e72fdd04bb6d938337cf8ac5254a
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-05-13 16:39:34 +0530

    Datatype Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// Primitive
+
+// 7 Types -> String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = trueconst;
+const outsideTemp = null;
+let userEmaik;
+
+// Reference  (NonPrimitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["Spiderman", "Ironman", "Thor"];
+
+let myObj = {
+  name: "Kuldeep",
+  age: 21,
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+};
