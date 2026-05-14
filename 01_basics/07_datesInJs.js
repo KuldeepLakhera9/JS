e2ff54a248a3366500c0a54cd74d399c1f32ebab
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-05-14 21:44:33 +0530

    Date

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,21 @@
+// Dares
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getTime());
