c7e3a833e75dc1b8bfcb231f9c58aa6785d42d84
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-05-16 08:51:48 +0530

    Array part 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,34 @@
+const marvel_heros = ["spiderman", "thor", "ironman"]
+const dc_heros = ["batman", "superman", "wonderwoman"]
+
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros);
+
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+console.log(all_new_heros);
+
+const another_array = [1,2,3,4,[5,6,7],8,[9,10,[4,5,6]]];
+
+const real_another_array = another_array.flat(Infinity);
+
+console.log(real_another_array);
+
+console.log(Array.isArray("Kuldeep"));
+
+console.log(Array.from("Kuldeep"));
+
+console.log(Array.from({name: "Kuldeep"}));  // Interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
