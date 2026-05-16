e75fc332f265f1eb55ff5894451fcb9686cebb22
KuldeepLakhera9 <kuldeeplakhera018@gmail.com>
2026-05-16 07:42:35 +0530

    Array Part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+const myArray = [1, 2, 3, 4, 5];
+// console.log(myArray[0]);
+
+const heros = ["spiderman", "batman", "superman"];
+
+// console.log(heros[2]);
+
+const myArray2 = new Array(1, 2, 3, 4, 5);
+//console.log(myArray2[1]);
+
+// Array Methods
+
+
+// myArray.push(6);
+// myArray.push(7);
+// console.log(myArray);
+
+// myArray.pop();
+
+// myArray.unshift(9);
+
+console.log(myArray.includes(4));
+console.log(myArray.indexOf(4));
+console.log(myArray);
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JS
 Repo for JavaScript
+
+## 01_basics
+
+### 1. Variables (var, let, const)
+### 2. Data Types (Primitive and Non-Primitive)
+### 3. Strings (String, String methods)
+### 4. Numbers and Math (Number, Math, Dates)
+
